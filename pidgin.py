bc893abeddeee0688801ec8e1e30c79cd111b70f
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-23 23:48:01 +0600

    Update pidgin.py

--- pidgin.py
+++ pidgin.py
@@ -1,23 +1,25 @@
+
+access_token='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'
+
+
+
 import streamlit as st
 import pandas as pd
 import requests
-#response = requests.get(url)
-
-#Forking starts here
 
 # Set your GitHub username and personal access token
-username = "DhrobePy"
-access_token='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'
-
-# Set the API endpoint for forking a repository
-api_url = "https://api.github.com/repos/DhrobePy/expenditure/forks"
+username = "your_username"
+access_token = "your_access_token"
 
 # Set the owner and repository name for the repository you want to fork
 owner = "DhrobePy"
 repo = "expenditure"
 
 # Set the forked repository name
-forked_repo = "expenditure"
+forked_repo = "expenditure_fork"
+
+# Set the API endpoint for forking a repository
+api_url = "https://api.github.com/repos/{owner}/{repo}/forks"
 
 # Set the headers for the API request
 headers = {"Authorization": f"Token {access_token}"}
@@ -30,33 +32,13 @@ response = requests.post(api_url.format(owner=owner, repo=repo), headers=headers
 
 # Check if the fork was successful
 if response.status_code == 202:
-    print("Repository forked successfully!")
+    st.success("Repository forked successfully!")
 else:
-    print("Error forking repository: ", response.text)
+    st.error("Error forking repository: ", response.text)
 
-
-
-
-
-
-
-
-#Forking ends here
-
-
-
-
-
-# Set the URL of the Excel file in the Git repository
-file_url = "Expenditure.xlsx"
-update_file="extra.xlsx"
-#response = requests.get(file_url)
-# Read the Excel file into a pandas DataFrame
-df = pd.read_excel(file_url)
-
-st.write(df)
-
-#st.set_page_config(page_title="Add Data to Excel Sheet")
+# Set the URL of the Excel file in the forked repository
+file_url = f"https://raw.githubusercontent.com/{username}/{forked_repo}/main/expenditures.xlsx"
+update_file = f"https://raw.githubusercontent.com/{username}/{forked_repo}/main/extras.xlsx"
 
 # Define a function to add data to the Excel sheet
 def add_data_to_excel(date, item_name, quantity):
@@ -70,6 +52,13 @@ def add_data_to_excel(date, item_name, quantity):
     # Write the updated DataFrame back to the Excel file
     df.to_excel(update_file, index=False)
 
+    # Show a success message
+    st.success("Data added to Excel sheet!")
+    
+    # Reload the updated DataFrame and display it in Streamlit
+    df1 = pd.read_excel(file_url)
+    st.write(df1)
+
 # Create the Streamlit app
 st.title("Add Data to Excel Sheet")
 
@@ -81,7 +70,5 @@ quantity = st.number_input("Quantity", min_value=1)
 # Create a button to add the data to the Excel file
 if st.button("Add Data"):
     add_data_to_excel(date, item_name, quantity)
-    st.success("Data added to Excel sheet!")
-    df1 = pd.read_excel(file_url)
-    st.write(df1)
+
     
