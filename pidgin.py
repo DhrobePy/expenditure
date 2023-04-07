14b500e6b805dc23490ad3eb4055c2af3c513d1a
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-04-08 05:10:35 +0600

    Update pidgin.py

--- pidgin.py
+++ pidgin.py
@@ -2,41 +2,65 @@ import streamlit as st
 import pandas as pd
 import datetime
 
-# Simple dictionary for storing user information (for demonstration purposes only)
+# Dictionary for storing user information (for demonstration purposes only)
 USERS = {
-    "user1": {"password": "pass1"},
-    "user2": {"password": "pass2"},
+    "admin": {"password": "admin101"},
+    "maker1": {"password": "maker123"},
+    "user3": {"password": "user3pass"},
+    "user4": {"password": "user4pass"},
 }
 
+def admin_dashboard():
+    st.title("Admin Dashboard")
+
+    if "expenses_to_authorize" not in st.session_state:
+        st.session_state.expenses_to_authorize = []
+
+    if "authorized_expenses" not in st.session_state:
+        st.session_state.authorized_expenses = []
+
+    expenses_to_authorize_df = pd.DataFrame(st.session_state.expenses_to_authorize, columns=["Username", "Category", "Amount", "Date", "Method"])
+    authorized_expenses_df = pd.DataFrame(st.session_state.authorized_expenses, columns=["Username", "Category", "Amount", "Date", "Method"])
+
+    if not expenses_to_authorize_df.empty:
+        st.subheader("Expenses to Authorize")
+        st.write(expenses_to_authorize_df)
+        if st.button("Authorize All"):
+            st.session_state.authorized_expenses.extend(st.session_state.expenses_to_authorize)
+            st.session_state.expenses_to_authorize = []
+            st.success("All expenses authorized")
+
+    if not authorized_expenses_df.empty:
+        st.subheader("Authorized Expenses")
+        st.write(authorized_expenses_df)
+
+def user_dashboard():
+    st.title("User Dashboard")
+
+    expense_category = st.selectbox("Expense Category", ["", "Travel", "Food", "Office Supplies", "Rent", "Utilities", "Miscellaneous"])
+    amount = st.number_input("Amount", min_value=0.0, step=0.1)
+    expense_date = st.date_input("Expense Date", datetime.date.today())
+    expense_method = st.selectbox("Expense Method", ["", "Cash", "Credit Card", "Debit Card", "Bank Transfer"])
+
+    if st.button("Submit Expense"):
+        if expense_category and amount > 0 and expense_method:
+            new_expense = {
+                "Username": st.session_state.username,
+                "Category": expense_category,
+                "Amount": amount,
+                "Date": expense_date,
+                "Method": expense_method,
+            }
+            st.session_state.expenses_to_authorize.append(new_expense)
+            st.success("Expense submitted")
+        else:
+            st.error("Please fill in all fields")
+
 def dashboard():
-    st.title("Dashboard")
-
-    # Create a container for the username and login time
-    user_info_container = st.container()
-
-    # Get the current time and format it
-    login_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # Add custom CSS to align the username and login time to the right
-    user_info_style = """
-    <style>
-        .user-info {
-            position: fixed;
-            top: 10px;
-            right: 10px;
-            text-align: right;
-        }
-    </style>
-    """
-
-    # Display the username and login time using the custom CSS
-    user_info_container.markdown(
-        f'<div class="user-info">Logged in as: {st.session_state.username}<br>Login Time: {login_time}</div>{user_info_style}',
-        unsafe_allow_html=True,
-    )
-
-    st.write("Welcome to the dashboard!")
-    # Add your dashboard content here
+    if st.session_state.username == "admin":
+        admin_dashboard()
+    else:
+        user_dashboard()
 
 def login_page():
     st.title("Login")
