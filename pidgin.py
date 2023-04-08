5514982667c30879d5e6f8a1b073346d4dcad2d0
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-04-08 14:57:37 +0600

    Update pidgin.py

--- pidgin.py
+++ pidgin.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 cred = credentials.Certificate("Exmod.json")
-firebase_admin.initialize_app(cred, {
+#firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://your-project-id.firebaseio.com/'
 })
 
