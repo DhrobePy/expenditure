5ba54438e319fadebe4668e0aec026dea4709f3b
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-04-09 20:33:15 +0600

    Update expense_function.py

--- expense_function.py
+++ expense_function.py
@@ -1,4 +1,16 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin.exceptions import NotFoundError
+from firebase_admin import firestore
+from streamlit_option_menu import option_menu
 
+
+cred = credentials.Certificate("Exmod.json")
+#firebase_admin.initialize_app(cred, {'databaseURL': 'https://your-project-id.firebaseio.com/'})
+db = firestore.client()
 
 
 def get_user_expenses(username):
